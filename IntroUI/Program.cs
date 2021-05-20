9a8f41c655c0c29b7c6c9333e9457c660d501b41
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-05-20 14:13:38 +1200

    init commit for c# setup and helloworld

---
+++ IntroUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using IntroLibrary;
+
+namespace IntroUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel p = new PersonModel
+            {
+                FirstName = "Obi Wan",
+                LastName = "Kenobi"
+            };
+
+            System.Console.WriteLine($"Jedi Master { p.FirstName } { p.LastName }");
+
+            Console.WriteLine("Hello There!");
+            System.Console.WriteLine("General Kenobi");
+            System.Console.WriteLine("You are a bold one");
+        }
+    }
+}
